5f79327a899be2cedfe68bce667cf42d0d115f09
Andrea Polanco <alpm9615@gmail.com>
2016-10-18 00:59:14 -0400

    Hola Git

---
+++ PruebasUnitariasII/src/PaqutePrincipal/ClasePrincipal.java
@@ -0,0 +1,12 @@
+package PaqutePrincipal;
+
+public class ClasePrincipal {
+	
+	
+	public long suma(long n, long m){
+		
+		return n+m;
+	}
+
+
+}
---
+++ PruebasUnitariasII/src/TestPackage/Test.java
@@ -0,0 +1,17 @@
+package TestPackage;
+
+import static org.junit.Assert.*;
+
+import PaqutePrincipal.ClasePrincipal;
+
+public class Test {
+	ClasePrincipal cp = new ClasePrincipal();
+
+	@org.junit.Test
+	public void testSum() {
+		long n = 5;
+		long m = 6;
+		assertEquals(cp.suma(n , m), 11);
+	}
+
+}
